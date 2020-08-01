0bff589d96cc619fa900c2f6d2690a6ece2ce38c
Mammad Mammadli <memmed199@gmail.com>
2020-08-01 18:10:46 +0400

    Implement css

--- src/modules/login/components/login.jsx
+++ src/modules/login/components/login.jsx
@@ -3,6 +3,11 @@ import { useForm } from "react-hook-form";
 import './index.scss';
 import { useDispatch } from 'react-redux';
 import { login } from '../store/actions';
+import { ReactComponent as Vogel } from '../../../assets/images/vogel.svg';
+import { ReactComponent as Globus } from '../../../assets/images/globus.svg';
+import { ReactComponent as Pipes } from '../../../assets/images/pipes.svg';
+import { ReactComponent as Calendar } from '../../../assets/images/calendar.svg';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const dispatch = useDispatch()
@@ -27,46 +32,69 @@ const Login = () => {
 
     return (
         <div className='loginPage'>
-            <div className='loginPage__container'>
-                {isLoading && (
-                    <div>loading...</div>
-                )}
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        name="email"
-                        placeholder="e-mail"
-                        type="text"
-                        ref={register({
-                            required: true,
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "invalid email address"
-                            }
-                        })}
-                    />
-                    {isTouched.isEmailTouched && errors.email && errors.email.message}
-                    <input
-                        name="password"
-                        type="password"
-                        onKeyPress={(e) => {
-                            if (e.key === 'Enter') {
-                                setTouched({ ...isTouched, isPasswordTouched: true });
-                            } else if (isTouched.isPasswordTouched) {
-                                setTouched({ ...isTouched, isPasswordTouched: false });
-                            }
-                        }}
-                        onBlur={() => setTouched({ ...isTouched, isPasswordTouched: true })}
-                        ref={register({
-                            required: true,
-                            minLength: {
-                                value: 6,
-                                message: 'Password length should be longer than 6'
-                            }
-                        })}
-                    />
-                    {isTouched.isPasswordTouched && errors.password && errors.password.message}
-                    <button type="submit">Submit</button>
-                </form>
+            <div className='loginPage__form'>
+                <div className='loginPage__form__top'>
+                    <span>Oh, hello</span>
+                </div>
+                <div className='loginPage__form__middle'>
+                    <div className='loginPage__form__icons'>
+                        <Calendar />
+                        <Globus />
+                        <Pipes />
+                    </div>
+                    <div className='loginPage__form__inputs'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className='loginPage__form__inputs__field'>
+                                <input
+                                    name="email"
+                                    placeholder="E-mail"
+                                    type="text"
+                                    ref={register({
+                                        required: true,
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "invalid email address"
+                                        }
+                                    })}
+                                />
+                            </div>
+                            <div className='loginPage__form__inputs__field'>
+                                <input
+                                    placeholder="Password"
+                                    name="password"
+                                    type="password"
+                                    onKeyPress={(e) => {
+                                        if (e.key === 'Enter') {
+                                            setTouched({ ...isTouched, isPasswordTouched: true });
+                                        } else if (isTouched.isPasswordTouched) {
+                                            setTouched({ ...isTouched, isPasswordTouched: false });
+                                        }
+                                    }}
+                                    onBlur={() => setTouched({ ...isTouched, isPasswordTouched: true })}
+                                    ref={register({
+                                        required: true,
+                                        minLength: {
+                                            value: 6,
+                                            message: 'Password length should be longer than 6'
+                                        }
+                                    })}
+                                />
+                            </div>
+                            <div className='loginPage__form__inputs__actions'>
+                                <button type="submit">Log In</button>
+                                <Link to='#'>Forgot your password?</Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className='loginPage__form__footer'>
+                    <span>&#169; Copyright 2020 Vogel, Inc. All rights reserved. Privacy Policy Terms</span>
+                </div>
+            </div>
+            <div className='loginPage__logo'>
+                <div className='loginPage__logo__circle'>
+                    <Vogel /> <span>Vogel</span>
+                </div>
             </div>
         </div>
     )
